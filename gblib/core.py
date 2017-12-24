ffaf507eb126425dc2830e32c3a20b2986cd3baf
Andrew Reilley <areill1337@gmail.com>
2017-12-23 20:04:19 -0500

    Started on decode

--- gblib/core.py
+++ gblib/core.py
@@ -3,15 +3,19 @@ from . import registers
 def add(opA, opB):
     return opA + opB
 
+def sub(opA, opB):
+    return opA - opB
 
+
+#op : [func, [args, string=reg, num=reladdr], dest]
 func = {
-        0x87 : [add, 'a', 'b'],
-        0x80 : [add, 'a', 'b'],
-        0x81 : [add, 'a', 'c'],
-        0x82 : [add, 'a', 'd'],
-        0x83 : [add, 'a', 'e'],
-        0x84 : [add, 'a', 'h'],
-        0x85 : [add, 'a', 'l']
+        0x87 : [add, ['a', 'b'], 'a'],
+        0x80 : [add, ['a', 'b'], 'a'],
+        0x81 : [add, ['a', 'c'], 'a'],
+        0x82 : [add, ['a', 'd'], 'a'],
+        0x83 : [add, ['a', 'e'], 'a'],
+        0x84 : [add, ['a', 'h'], 'a'],
+        0x85 : [add, ['a', 'l'], 'a']
       }
 
 class core():
@@ -19,18 +23,17 @@ class core():
         self.reg = registers.registers()
         
     def execute(self, inst):
-        self.reg.dumpState()
-        self.reg.setReg('A', 6)
-        self.reg.dumpState()
-        self.reg.setReg('C', 4)
-        self.reg.dumpState()
+        
         lookup = func[inst[0]]
         res = lookup[0](self.reg.getReg(lookup[1]), self.reg.getReg(lookup[2]))
         self.reg.setReg('a', res)
         self.reg.dumpState()
         
     
-    
+    def decode(self, opcode, index):
+        print(str(opcode) + " at " + str(index))
+        
+        self.execute(self, decoded)
     
     def parseROM(self, rom):
         self.rom = []
@@ -38,4 +41,4 @@ class core():
             cont = file.read()
             for c in cont:
                 self.rom.append(hex(c))
-        print(self.rom)
+        #print(self.rom)
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import gblib
 
 testCore = gblib.core()
 
-testCore.execute([0x81])
+testCore.reg.dumpState()
+testCore.reg.setReg('A', 6)
+testCore.reg.dumpState()
+testCore.reg.setReg('C', 4)
+testCore.reg.dumpState()
+
+testCore.decode(0x81, 0x0)
 
 testCore.parseROM("../roms/bios.gb")
