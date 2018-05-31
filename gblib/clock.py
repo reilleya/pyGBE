f9ef44a70c14d183654ecfb5ebbe0dfbe02c3bd9
Andrew Reilley <areill1337@gmail.com>
2018-05-31 19:01:02 -0400

    Added timer and timer interrupt

--- gblib/clock.py
+++ gblib/clock.py
@@ -3,18 +3,61 @@ class clock():
         self.core = core
         self.cycles = 0
         
+        self.divReg = 0
+        
+        self.timerEnabled = False
+        self.timerVal = 0
+        self.timerResetVal = 0
+        self.timerRate = 1024
+        
     def reset(self):
         self.cycles = 0
         
     def stepCycles(self, cycles):
         self.cycles += cycles
-        # Check timers
+        
+        if cycles % 256 == 0:
+            self.divReg += 1
+            if self.divReg > 0xFF:
+                self.divReg = 0
+        
+        if self.timerEnabled:
+            if self.cycles % self.timerRate == 0:
+                self.timerVal += 1
+            if self.timerVal > 0xFF:
+                self.timerVal = self.timerResetVal
+                self.core.int.trigger(2) # Trigger a timer interrupt
         
     def read(self, loc):
-        return 0
+        if loc == 0xFF04:
+            return self.divReg
+            
+        if loc == 0xFF05:
+            return self.timerVal
+    
+        if loc == 0xFF06: # Timer modulo
+            return self.timerResetVal
         
     def write(self, loc, value):
-        pass
+        if loc == 0xFF04:
+            self.divReg = 0
+    
+        if loc == 0xFF05:
+            self.timerVal = value
+    
+        if loc == 0xFF06: # Timer modulo
+            self.timerResetVal = value
+        
+        if loc == 0xFF07: # Timer control register
+            self.timerEnabled = value & 0b100
+            if value % 0b11 == 0:
+                self.timerRate = 1024
+            elif value % 0b11 == 1:
+                self.timerRate = 16
+            elif value % 0b11 == 2:
+                self.timerRate = 64
+            else:
+                self.timerRate = 256
         
     @property
     def time(self):
--- gblib/interrupts.py
+++ gblib/interrupts.py
@@ -8,21 +8,42 @@ class interrupts():
     def __init__(self, core):
         self.core = core
         self.enabled = True
+        
         self.vblankEnabled = False
+        self.vblankTrig = False
+        
         self.LCDCEnabled = False
+        self.LCDCTrig = False
+        
         self.timerEnabled = False
+        self.timerTrig = False
+        
         self.serialCompEnabled = False
+        self.serialCompTrig = False
+        
         self.edgeEnabled = False
+        self.edgeTrig = False
         
     def update(self):
         pass
         
     def read(self, loc):
+        if loc == 0xFF0F:
+            return combineBits(self.vblankTrig, self.LCDCTrig, self.timerTrig
+                                self.serialCompTrig, self.edgeTrig)
+        
         if loc == 0xFFFF:
             return combineBits(self.vblankEnabled, self.LCDCEnabled, self.timerEnabled,
                                 self.serialCompEnabled, self.edgeEnabled)
     
     def write(self, loc, value):
+        if loc == 0xFF0F:
+            self.vblankTrig = bool(value & 0b00001)
+            self.LCDCTrig = bool(value & 0b00010)
+            self.timerTrig = bool(value & 0b00100)
+            self.serialCompTrig = bool(value & 0b01000)
+            self.edgeTrig = bool(value & 0b10000)
+    
         if loc == 0xFFFF:
             self.vblankEnabled = bool(value & 0b00001)
             self.LCDCEnabled = bool(value & 0b00010)
@@ -35,8 +56,16 @@ class interrupts():
     
     def disable(self):
         self.enabled = False
-    
-    def callInt(self, loc):
-        self.enabled = False
         
-        self.core.reg.setReg("sp", loc)
+    def trigger(self, type):
+        if self.enabled:
+            jmpAddr = None
+            if type == 2: # Timer
+                if self.timerEnabled:
+                    self.timerTrig = True
+                    jmpAddr = 0x0050
+                    
+            if jmpAddr is not None:
+                self.enabled = False
+                self.core.push(self.core.reg.getReg("sp"))
+                self.core.reg.setReg("sp", jmpAddr)
