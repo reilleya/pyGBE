ab49196b8f9130f88ec0d434a3d7347aeca6375b
Andrew Reilley <areill1337@gmail.com>
2018-06-02 00:05:24 -0400

    Hides window by default and includes some display timing

--- gblib/display.py
+++ gblib/display.py
@@ -1,25 +1,41 @@
 import pygame, threading
 
 class display():
-    def __init__(self, core):
+    def __init__(self, core, openWindow = False):
         self.core = core
         
         self.ycoord = 0
         
+        self.vblank = False
+        
         #Window stuff
-        pygame.init()
-        self.window = pygame.display.set_mode([160, 144])
-        self.window.fill([255, 255, 255])
+        self.openWindow = openWindow
+        if self.openWindow:
+            pygame.init()
+            self.window = pygame.display.set_mode([160, 144])
+            self.window.fill([255, 255, 255])
         
     def update(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print("exit")
-        pygame.display.flip()
+        if self.core.clock.cycles % 456 == 0:
+            print("LY!")
+            self.ycoord += 1
+            if self.ycoord == 144:
+                print("VBLANK")
+            if self.ycoord == 155:
+                self.ycoord = 0
+                
+        
+        
+        if self.openWindow:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("exit")
+            pygame.display.flip()
         
     def read(self, loc):
         if loc == 0xFF44:
             return self.ycoord
     
     def write(self, loc, value):
-        pass
+        if loc == 0xFF44:
+            self.ycoord = 0
